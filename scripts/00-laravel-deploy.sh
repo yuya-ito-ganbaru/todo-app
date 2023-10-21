ece6dfb8ad0fd9758c4697e86c17977432815b51
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-10-21 18:46:45 +0900

    デプロイスクリプトの修正

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,10 +4,10 @@ composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
-sail artisan config:cache
+php artisan config:cache
 
 echo "Caching routes..."
-sail artisan route:cache
+php artisan route:cache
 
 echo "Running migrations..."
-sail artisan migrate --force
+php artisan migrate --force
